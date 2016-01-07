c707f59186592bf981d1be49f5895d7520114ee4
gareth4192 <gareth-123@live.com>
2016-01-07 15:57:17 +0000

    temperature can be increased or decreased

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,12 +2,22 @@ describe('Thermostat', function(){
 
   var thermostat;
 
-  beforeEach(function(){
+  beforeEach(function() {
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function(){
-    expect(thermostat.temp).toEqual(20);
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getTemp()).toEqual(20);
+  });
+
+  it('temp can be increased', function(){
+    thermostat.hotter();
+    expect(thermostat.getTemp()).toEqual(21);
+  });
+
+  it('temp can be decreaded', function(){
+    thermostat.colder();
+    expect(thermostat.getTemp()).toEqual(19);
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,3 +2,14 @@
 Thermostat = function(){
   this.temp = 20;
 }
+Thermostat.prototype.getTemp = function(){
+  return this.temp;
+};
+
+Thermostat.prototype.hotter = function(){
+  this.temp += 1;
+};
+
+Thermostat.prototype.colder = function(){
+  this.temp -= 1;
+};
