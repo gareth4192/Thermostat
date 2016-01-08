0c20a266374a1c5e082e6c3bb703ef82c9fc9e51
gareth4192 <gareth-123@live.com>
2016-01-08 18:23:30 +0000

    tests for usage levels added

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -27,15 +27,73 @@ describe('Thermostat', function(){
     expect(function(){thermostat.colder();}).toThrow(new Error("Minimum Temp reached."));
   });
 
-  it('has max temp', function() {
+  it('has max temp of 32 when psm is off', function() {
+    thermostat.psm = false;
+    expect(thermostat.isPsmOn()).toBe(false)
     for (i=0; i < 12; i++) {
         thermostat.hotter();
     }
     expect(function(){thermostat.hotter();}).toThrow(new Error("Maximum Temp reached."));
   });
 
+  it('has max temp of 25 when psm is on', function() {
+    for (i=0; i < 5; i++) {
+        thermostat.hotter();
+    }
+    expect(function(){thermostat.hotter();}).toThrow(new Error("Maximum Temp reached."));
+  });
+
+
   it('has power saving mode', function(){
     expect(thermostat.isPsmOn()).toBe(true)
   });
+
+  it('power saving mode can be turned off', function(){
+    thermostat.psmOff();
+    expect(thermostat.isPsmOn()).toBe(false)
+  });
+
+  it('power saving mode can be turned back on', function(){
+    thermostat.psmOff();
+    thermostat.psmOn();
+    expect(thermostat.isPsmOn()).toBe(true)
+  });
+
+  it('can be reset to starting temp', function(){
+    thermostat.hotter();
+    thermostat.resetTemp();
+    expect(thermostat.getTemp()).toEqual(20);
+  });
+
+
+  describe('displaying usage levels', function() {
+
+    describe('when the temperature is below 18 degrees', function() {
+      it('it is considered low-usage', function() {
+        for (var i = 0; i < 3; i++) {
+          thermostat.colder();
+        }
+        expect(thermostat.energyUsage()).toEqual('green');
+      });
+    });
+
+    describe('when the temperature is between 18 and 25 degrees', function() {
+      it('it is considered medium-usage', function() {
+        expect(thermostat.energyUsage()).toEqual('amber');
+      });
+    });
+
+    describe('when the temperature is anything else', function() {
+      it('it is considered high-usage', function() {
+        thermostat.psm = false;
+        for (var i = 0; i < 6; i++) {
+          thermostat.hotter();
+        }
+        expect(thermostat.energyUsage()).toEqual('red');
+      });
+    });
+  });
+
+
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -3,16 +3,27 @@ Thermostat = function(){
   this.STARTING_TEMP = 20;
   this.temp = this.STARTING_TEMP;
   this.MINIMUM_TEMP = 10;
+  this.PSM_MAXIMUM_TEMP = 25;
   this.MAXIMUM_TEMP = 32;
   this.psm = true;
+  this.MEDIUM_USAGE = 18;
 }
+
+
 Thermostat.prototype.getTemp = function(){
   return this.temp;
 };
 
 Thermostat.prototype.hotter = function(){
-  if (this.temp === this.MAXIMUM_TEMP) {
+  if (this.isPsmOn) {
+    if (this.temp === this.PSM_MAXIMUM_TEMP) {
     throw new Error("Maximum Temp reached.");
+    }
+  }
+  if (this.isPsmOn === false) {
+    if (this.temp === this.MAXIMUM_TEMP) {
+    throw new Error("Maximum Temp reached.");
+    }
   }
   this.temp += 1;
 };
@@ -27,3 +38,25 @@ if (this.temp === this.MINIMUM_TEMP) {
 Thermostat.prototype.isPsmOn = function(){
   return this.psm;
 };
+
+Thermostat.prototype.psmOff = function(){
+  this.psm = false;
+};
+
+Thermostat.prototype.psmOn = function(){
+  this.psm = true;
+};
+
+Thermostat.prototype.resetTemp = function(){
+  this.temp = this.STARTING_TEMP;
+};
+
+Thermostat.prototype.energyUsage = function() {
+  if (this.temp < this.MEDIUM_USAGE) {
+    return 'low-usage';
+  }
+  if (this.temp >= this.MEDIUM_USAGE && this.temperature <= this.PSM_MAXIMUM_TEMP) {
+    return 'medium-usage';
+  }
+  return 'high-usage';
+}
